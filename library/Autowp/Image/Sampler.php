ceb02630914b58281d266bc8804d44441958e482
Dmitry <dmitry@pereslegin.ru>
2013-09-03 17:33:02 +0400

    Other&maximum fit reduceOnly flag, tests

--- library/Autowp/Image/Sampler.php
+++ library/Autowp/Image/Sampler.php
@@ -3,6 +3,234 @@
 class Autowp_Image_Sampler
 {
     /**
+     * @param Imagick $imagick
+     * @param Autowp_Image_Sampler_Format $format
+     */
+    protected function _convertByInnerFit(Imagick $imagick,
+        Autowp_Image_Sampler_Format $format)
+    {
+        $srcWidth = $imagick->getImageWidth();
+        $srcHeight = $imagick->getImageHeight();
+        $srcRatio = $srcWidth / $srcHeight;
+
+        $widthLess  = $format->getWidth()  && ($srcWidth  < $format->getWidth() );
+        $heightLess = $format->getHeight() && ($srcHeight < $format->getHeight());
+        $sizeLess = $widthLess || $heightLess;
+
+        $ratio = $format->getWidth() / $format->getHeight();
+
+        if ($format->getReduceOnly() && $sizeLess) {
+            // dont crop
+            if (!$heightLess) {
+                // resize by height
+                $scaleHeight = $format->getHeight();
+                $scaleWidth = round($scaleHeight * $srcRatio);
+                $imagick->scaleImage(
+                    $scaleWidth, $scaleHeight, false
+                );
+            } elseif (!$widthLess) {
+                // resize by width
+                $scaleWidth = $format->getWidth();
+                $scaleHeight = round($scaleWidth / $srcRatio);
+                $imagick->scaleImage(
+                    $scaleWidth, $scaleHeight, false
+                );
+            }
+        } else {
+
+            // высчитываем размеры обрезания
+            if ($ratio < $srcRatio) {
+                // широкая картинка
+                $cropWidth = (int)round($srcHeight * $ratio);
+                $cropHeight = $srcHeight;
+                $cropLeft = (int)floor(($srcWidth - $cropWidth) / 2);
+                $cropTop = 0;
+            } else {
+                // высокая картинка
+                $cropWidth = $srcWidth;
+                $cropHeight = (int)round($srcWidth / $ratio);
+                $cropLeft = 0;
+                $cropTop = (int)floor(($srcHeight - $cropHeight) / 2);
+            }
+
+            $imagick->setImagePage(0, 0, 0, 0);
+            if (!$imagick->cropImage($cropWidth, $cropHeight, $cropLeft, $cropTop)) {
+                return $this->_raise("Error crop");
+            }
+
+            $imagick->scaleImage(
+                $format->getWidth(), $format->getHeight(), false
+            );
+        }
+    }
+
+    /**
+     * @param Imagick $imagick
+     * @param Autowp_Image_Sampler_Format $format
+     */
+    protected function _convertByOuterFit(Imagick $imagick,
+        Autowp_Image_Sampler_Format $format)
+    {
+        $srcWidth = $imagick->getImageWidth();
+        $srcHeight = $imagick->getImageHeight();
+        $srcRatio = $srcWidth / $srcHeight;
+
+        $widthLess  = $format->getWidth()  && ($srcWidth  < $format->getWidth() );
+        $heightLess = $format->getHeight() && ($srcHeight < $format->getHeight());
+        $sizeLess = $widthLess || $heightLess;
+
+        $ratio = $format->getWidth() / $format->getHeight();
+
+        if ($format->getReduceOnly() && $sizeLess) {
+            // dont crop
+            if (!$heightLess) {
+                // resize by height
+                $scaleHeight = $format->getHeight();
+                $scaleWidth = round($scaleHeight * $srcRatio);
+                $imagick->scaleImage(
+                    $scaleWidth, $scaleHeight, false
+                );
+            } elseif (!$widthLess) {
+                // resize by width
+                $scaleWidth = $format->getWidth();
+                $scaleHeight = round($scaleWidth / $srcRatio);
+                $imagick->scaleImage(
+                    $scaleWidth, $scaleHeight, false
+                );
+            }
+        } else {
+            // высчитываем размеры обрезания
+            if ($ratio < $srcRatio) {
+                $scaleWidth = $format->getWidth();
+                $scaleHeight = round($format->getWidth() / $srcRatio);// добавляем поля сверху и снизу
+            } else {
+                // добавляем поля по бокам
+                $scaleWidth = round($format->getHeight() * $srcRatio);
+                $scaleHeight = $format->getHeight();
+            }
+
+            $imagick->scaleImage(
+                $scaleWidth, $scaleHeight, false
+            );
+        }
+
+        // extend by bg-space
+        $borderLeft = floor(($format->getWidth()  - $imagick->getImageWidth())  / 2);
+        $borderTop  = floor(($format->getHeight() - $imagick->getImageHeight()) / 2);
+
+        $imagick->extentImage(
+            $format->getWidth(),
+            $format->getHeight(),
+            -$borderLeft,
+            -$borderTop
+        );
+    }
+
+    /**
+     * @param Imagick $imagick
+     * @param Autowp_Image_Sampler_Format $format
+     */
+    protected function _convertByMaximumFit(Imagick $imagick,
+        Autowp_Image_Sampler_Format $format)
+    {
+        $srcWidth = $imagick->getImageWidth();
+        $srcHeight = $imagick->getImageHeight();
+        $srcRatio = $srcWidth / $srcHeight;
+
+        $widthLess  = $format->getWidth()  && ($srcWidth  < $format->getWidth() );
+        $heightLess = $format->getHeight() && ($srcHeight < $format->getHeight());
+        $sizeLess = $widthLess || $heightLess;
+
+        $ratio = $format->getWidth() / $format->getHeight();
+
+        if ($format->getReduceOnly() && $sizeLess) {
+
+            if (!$heightLess) {
+                // resize by height
+                $scaleHeight = $format->getHeight();
+                $scaleWidth = round($scaleHeight * $srcRatio);
+                $imagick->scaleImage(
+                    $scaleWidth, $scaleHeight, false
+                );
+            } elseif (!$widthLess) {
+                // resize by width
+                $scaleWidth = $format->getWidth();
+                $scaleHeight = round($scaleWidth / $srcRatio);
+                $imagick->scaleImage(
+                    $scaleWidth, $scaleHeight, false
+                );
+            }
+
+        } else {
+
+            // высчитываем размеры обрезания
+            if ($ratio < $srcRatio) {
+                $scaleWidth = $format->getWidth();
+                $scaleHeight = round($format->getWidth() / $srcRatio);
+            } else {
+                // добавляем поля по бокам
+                $scaleWidth = round($format->getHeight() * $srcRatio);
+                $scaleHeight = $format->getHeight();
+            }
+
+            $imagick->scaleImage(
+                $scaleWidth, $scaleHeight, false
+            );
+
+        }
+    }
+
+    /**
+     * @param Imagick $imagick
+     * @param Autowp_Image_Sampler_Format $format
+     */
+    protected function _convertByWidth(Imagick $imagick,
+        Autowp_Image_Sampler_Format $format)
+    {
+        $srcWidth = $imagick->getImageWidth();
+        $srcRatio = $srcWidth / $imagick->getImageHeight();
+
+        $widthLess = $srcWidth < $format->getWidth();
+
+        if ($format->getReduceOnly() && $widthLess) {
+            $scaleWidth = $srcWidth;
+        } else {
+            $scaleWidth = $format->getWidth();
+        }
+
+        $scaleHeight = round($scaleWidth / $srcRatio);
+
+        $imagick->scaleImage($scaleWidth, $scaleHeight, false);
+    }
+
+    /**
+     * @param Imagick $imagick
+     * @param Autowp_Image_Sampler_Format $format
+     */
+    protected function _convertByHeight(Imagick $imagick,
+        Autowp_Image_Sampler_Format $format)
+    {
+        $srcHeight = $imagick->getImageHeight();
+        $srcRatio = $imagick->getImageWidth() / $srcHeight;
+
+        $heightLess = $format->getHeight() && ($srcHeight < $format->getHeight());
+
+        $ratio = $format->getWidth() / $format->getHeight();
+
+        if ($format->getReduceOnly() && $heightLess) {
+            $scaleHeight = $srcHeight;
+        } else {
+            $scaleHeight = $format->getHeight();
+        }
+
+        $scaleWidth = round($scaleHeight * $srcRatio);
+
+        $imagick->scaleImage(
+            $scaleWidth, $scaleHeight, false
+        );
+    }
+
+    /**
      * @param Imagick $source
      * @param array|Autowp_Image_Sampler_Format $format
      * @throws Autowp_Image_Sampler_Exception
@@ -16,7 +244,6 @@ class Autowp_Image_Sampler
                 return $this->_raise("Unexpected type of format");
             }
         }
-
 
         $crop = $format->getCrop();
 
@@ -64,133 +291,30 @@ class Autowp_Image_Sampler
         }
         $imagick->setBackgroundColor($bg);
 
-        $srcWidth = $imagick->getImageWidth();
-        $srcHeight = $imagick->getImageHeight();
-        $srcRatio = $srcWidth / $srcHeight;
-
-        $widthLess  = $format->getWidth()  && ($srcWidth  < $format->getWidth() );
-        $heightLess = $format->getHeight() && ($srcHeight < $format->getHeight());
-        $sizeLess = $widthLess || $heightLess;
 
         if ($format->getWidth() && $format->getHeight()) {
-            $ratio = $format->getWidth() / $format->getHeight();
-
             switch ($format->getFitType()) {
                 case Autowp_Image_Sampler_Format::FIT_TYPE_INNER:
-
-                    if ($format->getReduceOnly() && $sizeLess) {
-                        // dont crop
-                        if (!$heightLess) {
-                            // resize by height
-                            $scaleHeight = $format->getHeight();
-                            $scaleWidth = round($scaleHeight * $srcRatio);
-                            $imagick->scaleImage(
-                                $scaleWidth, $scaleHeight, false
-                            );
-                        } elseif (!$widthLess) {
-                            // resize by width
-                            $scaleWidth = $format->getWidth();
-                            $scaleHeight = round($scaleWidth / $srcRatio);
-                            $imagick->scaleImage(
-                                $scaleWidth, $scaleHeight, false
-                            );
-                        }
-                    } else {
-
-                        // высчитываем размеры обрезания
-                        if ($ratio < $srcRatio) {
-                            // широкая картинка
-                            $cropWidth = (int)round($srcHeight * $ratio);
-                            $cropHeight = $srcHeight;
-                            $cropLeft = (int)floor(($srcWidth - $cropWidth) / 2);
-                            $cropTop = 0;
-                        } else {
-                            // высокая картинка
-                            $cropWidth = $srcWidth;
-                            $cropHeight = (int)round($srcWidth / $ratio);
-                            $cropLeft = 0;
-                            $cropTop = (int)floor(($srcHeight - $cropHeight) / 2);
-                        }
-
-                        $imagick->setImagePage(0, 0, 0, 0);
-                        if (!$imagick->cropImage($cropWidth, $cropHeight, $cropLeft, $cropTop)) {
-                            return $this->_raise("Error crop");
-                        }
-
-                        $imagick->scaleImage(
-                            $format->getWidth(), $format->getHeight(), false
-                        );
-                    }
-
+                    $this->_convertByInnerFit($imagick, $format);
                     break;
 
                 case Autowp_Image_Sampler_Format::FIT_TYPE_OUTER:
-
-                    // высчитываем размеры обрезания
-                    if ($ratio < $srcRatio) {
-                        $scaleWidth = $format->getWidth();
-                        $scaleHeight = round($format->getWidth() / $srcRatio);// добавляем поля сверху и снизу
-                    } else {
-                        // добавляем поля по бокам
-                        $scaleWidth = round($format->getHeight() * $srcRatio);
-                        $scaleHeight = $format->getHeight();
-                    }
-
-                    $imagick->scaleImage(
-                        $scaleWidth, $scaleHeight, false
-                    );
-
-                    $borderLeft = floor(($format->getWidth() - $scaleWidth) / 2);
-                    $borderTop = floor(($format->getHeight() - $scaleHeight) / 2);
-
-                    $imagick->extentImage(
-                        $format->getWidth(),
-                        $format->getHeight(),
-                        -$borderLeft,
-                        -$borderTop
-                    );
-
+                    $this->_convertByOuterFit($imagick, $format);
                     break;
 
                 case Autowp_Image_Sampler_Format::FIT_TYPE_MAXIMUM:
-
-                    // высчитываем размеры обрезания
-                    if ($ratio < $srcRatio) {
-                        $scaleWidth = $format->getWidth();
-                        $scaleHeight = round($format->getWidth() / $srcRatio);
-                    } else {
-                        // добавляем поля по бокам
-                        $scaleWidth = round($format->getHeight() * $srcRatio);
-                        $scaleHeight = $format->getHeight();
-                    }
-
-                    $imagick->scaleImage(
-                        $scaleWidth, $scaleHeight, false
-                    );
-
+                    $this->_convertByMaximumFit($imagick, $format);
                     break;
 
                 default:
-                    $this->_raise("Неизвестный FIT_TYPE `{$format->getFitType()}`");
+                    $this->_raise("Unexpected FIT_TYPE `{$format->getFitType()}`");
             }
         } else {
 
             if ($format->getWidth()) {
-                $scaleWidth = $format->getWidth();
-                $scaleHeight = round($format->getWidth() / $srcRatio);
-
-                $imagick->scaleImage(
-                    $scaleWidth, $scaleHeight, false
-                );
-
+                $this->_convertByWidth($imagick, $format);
             } elseif ($format->getHeight()) {
-                // добавляем поля по бокам
-                $scaleWidth = round($format->getHeight() * $srcRatio);
-                $scaleHeight = $format->getHeight();
-
-                $imagick->scaleImage(
-                    $scaleWidth, $scaleHeight, false
-                );
+                $this->_convertByHeight($imagick, $format);
             }
 
         }
--- tests/Autowp/Image/SamplerTests.php
+++ tests/Autowp/Image/SamplerTests.php
@@ -167,6 +167,341 @@ class Autowp_Image_SamplerTests extends PHPUnit_Framework_TestCase
         $this->assertSame($imagick->getImageHeight(), 100);
         $imagick->clear();
     }
+
+    public function testReduceOnlyWithOuterFitWorks()
+    {
+        $sampler = new Autowp_Image_Sampler();
+
+        $file = dirname(__FILE__) . '/_files/Towers_Schiphol_small.jpg';
+
+        $imagick = new Imagick();
+
+        // both size less
+        $imagick->readImage($file); //101x149
+        $sampler->convertImagick($imagick, array(
+            'fitType'    => Autowp_Image_Sampler_Format::FIT_TYPE_OUTER,
+            'width'      => 150,
+            'height'     => 200,
+            'reduceOnly' => true
+        ));
+
+        $this->assertSame($imagick->getImageWidth(), 150);
+        $this->assertSame($imagick->getImageHeight(), 200);
+        $imagick->clear();
+
+        // width less
+        $imagick->readImage($file); //101x149
+        $sampler->convertImagick($imagick, array(
+            'fitType'    => Autowp_Image_Sampler_Format::FIT_TYPE_OUTER,
+            'width'      => 150,
+            'height'     => 100,
+            'reduceOnly' => true
+        ));
+
+        $this->assertSame($imagick->getImageWidth(), 150);
+        $this->assertSame($imagick->getImageHeight(), 100);
+        $imagick->clear();
+
+        // height less
+        $imagick->readImage($file); //101x149
+        $sampler->convertImagick($imagick, array(
+            'fitType'    => Autowp_Image_Sampler_Format::FIT_TYPE_OUTER,
+            'width'      => 50,
+            'height'     => 200,
+            'reduceOnly' => true
+        ));
+
+        $this->assertSame($imagick->getImageWidth(), 50);
+        $this->assertSame($imagick->getImageHeight(), 200);
+        $imagick->clear();
+
+        // not less
+        $imagick->readImage($file); //101x149
+        $sampler->convertImagick($imagick, array(
+            'fitType'    => Autowp_Image_Sampler_Format::FIT_TYPE_OUTER,
+            'width'      => 50,
+            'height'     => 100,
+            'reduceOnly' => true
+        ));
+
+        $this->assertSame($imagick->getImageWidth(), 50);
+        $this->assertSame($imagick->getImageHeight(), 100);
+        $imagick->clear();
+
+
+        // both size less, reduceOnly off
+        $imagick->readImage($file); //101x149
+        $sampler->convertImagick($imagick, array(
+            'fitType'    => Autowp_Image_Sampler_Format::FIT_TYPE_OUTER,
+            'width'      => 150,
+            'height'     => 200,
+            'reduceOnly' => false
+        ));
+
+        $this->assertSame($imagick->getImageWidth(), 150);
+        $this->assertSame($imagick->getImageHeight(), 200);
+        $imagick->clear();
+
+        // width less, reduceOnly off
+        $imagick->readImage($file); //101x149
+        $sampler->convertImagick($imagick, array(
+            'fitType'    => Autowp_Image_Sampler_Format::FIT_TYPE_OUTER,
+            'width'      => 150,
+            'height'     => 100,
+            'reduceOnly' => false
+        ));
+
+        $this->assertSame($imagick->getImageWidth(), 150);
+        $this->assertSame($imagick->getImageHeight(), 100);
+        $imagick->clear();
+
+        // height less, reduceOnly off
+        $imagick->readImage($file); //101x149
+        $sampler->convertImagick($imagick, array(
+            'fitType'    => Autowp_Image_Sampler_Format::FIT_TYPE_OUTER,
+            'width'      => 50,
+            'height'     => 200,
+            'reduceOnly' => false
+        ));
+
+        $this->assertSame($imagick->getImageWidth(), 50);
+        $this->assertSame($imagick->getImageHeight(), 200);
+        $imagick->clear();
+
+        // not less, reduceOnly off
+        $imagick->readImage($file); //101x149
+        $sampler->convertImagick($imagick, array(
+            'fitType'    => Autowp_Image_Sampler_Format::FIT_TYPE_OUTER,
+            'width'      => 50,
+            'height'     => 100,
+            'reduceOnly' => false
+        ));
+
+        $this->assertSame($imagick->getImageWidth(), 50);
+        $this->assertSame($imagick->getImageHeight(), 100);
+        $imagick->clear();
+    }
+
+    public function testReduceOnlyWithMaximumFitWorks()
+    {
+        $sampler = new Autowp_Image_Sampler();
+
+        $file = dirname(__FILE__) . '/_files/Towers_Schiphol_small.jpg';
+
+        $imagick = new Imagick();
+
+        // both size less
+        $imagick->readImage($file); //101x149
+        $sampler->convertImagick($imagick, array(
+            'fitType'    => Autowp_Image_Sampler_Format::FIT_TYPE_MAXIMUM,
+            'width'      => 150,
+            'height'     => 200,
+            'reduceOnly' => true
+        ));
+
+        $this->assertSame($imagick->getImageWidth(), 101);
+        $this->assertSame($imagick->getImageHeight(), 149);
+        $imagick->clear();
+
+        // width less
+        $imagick->readImage($file); //101x149
+        $sampler->convertImagick($imagick, array(
+            'fitType'    => Autowp_Image_Sampler_Format::FIT_TYPE_MAXIMUM,
+            'width'      => 150,
+            'height'     => 100,
+            'reduceOnly' => true
+        ));
+
+        $this->assertSame($imagick->getImageWidth(), 68);
+        $this->assertSame($imagick->getImageHeight(), 100);
+        $imagick->clear();
+
+        // height less
+        $imagick->readImage($file); //101x149
+        $sampler->convertImagick($imagick, array(
+            'fitType'    => Autowp_Image_Sampler_Format::FIT_TYPE_MAXIMUM,
+            'width'      => 50,
+            'height'     => 200,
+            'reduceOnly' => true
+        ));
+
+        $this->assertSame($imagick->getImageWidth(), 50);
+        $this->assertSame($imagick->getImageHeight(), 74);
+        $imagick->clear();
+
+        // not less
+        $imagick->readImage($file); //101x149
+        $sampler->convertImagick($imagick, array(
+            'fitType'    => Autowp_Image_Sampler_Format::FIT_TYPE_MAXIMUM,
+            'width'      => 50,
+            'height'     => 100,
+            'reduceOnly' => true
+        ));
+
+        $this->assertSame($imagick->getImageWidth(), 50);
+        $this->assertSame($imagick->getImageHeight(), 74);
+        $imagick->clear();
+
+
+        // both size less, reduceOnly off
+        $imagick->readImage($file); //101x149
+        $sampler->convertImagick($imagick, array(
+            'fitType'    => Autowp_Image_Sampler_Format::FIT_TYPE_MAXIMUM,
+            'width'      => 150,
+            'height'     => 200,
+            'reduceOnly' => false
+        ));
+
+        $this->assertSame($imagick->getImageWidth(), 136);
+        $this->assertSame($imagick->getImageHeight(), 200);
+        $imagick->clear();
+
+        // width less, reduceOnly off
+        $imagick->readImage($file); //101x149
+        $sampler->convertImagick($imagick, array(
+            'fitType'    => Autowp_Image_Sampler_Format::FIT_TYPE_MAXIMUM,
+            'width'      => 150,
+            'height'     => 100,
+            'reduceOnly' => false
+        ));
+
+        $this->assertSame($imagick->getImageWidth(), 68);
+        $this->assertSame($imagick->getImageHeight(), 100);
+        $imagick->clear();
+
+        // height less, reduceOnly off
+        $imagick->readImage($file); //101x149
+        $sampler->convertImagick($imagick, array(
+            'fitType'    => Autowp_Image_Sampler_Format::FIT_TYPE_MAXIMUM,
+            'width'      => 50,
+            'height'     => 200,
+            'reduceOnly' => false
+        ));
+
+        $this->assertSame($imagick->getImageWidth(), 50);
+        $this->assertSame($imagick->getImageHeight(), 74);
+        $imagick->clear();
+
+        // not less, reduceOnly off
+        $imagick->readImage($file); //101x149
+        $sampler->convertImagick($imagick, array(
+            'fitType'    => Autowp_Image_Sampler_Format::FIT_TYPE_MAXIMUM,
+            'width'      => 50,
+            'height'     => 100,
+            'reduceOnly' => false
+        ));
+
+        $this->assertSame($imagick->getImageWidth(), 50);
+        $this->assertSame($imagick->getImageHeight(), 74);
+        $imagick->clear();
+    }
+
+    public function testReduceOnlyByWidthWorks()
+    {
+        $sampler = new Autowp_Image_Sampler();
+
+        $file = dirname(__FILE__) . '/_files/Towers_Schiphol_small.jpg';
+
+        $imagick = new Imagick();
+
+        // width less
+        $imagick->readImage($file); //101x149
+        $sampler->convertImagick($imagick, array(
+            'width'      => 150,
+            'reduceOnly' => true
+        ));
+
+        $this->assertSame($imagick->getImageWidth(), 101);
+        $this->assertSame($imagick->getImageHeight(), 149);
+        $imagick->clear();
+
+        // not less
+        $imagick->readImage($file); //101x149
+        $sampler->convertImagick($imagick, array(
+            'width'      => 50,
+            'reduceOnly' => true
+        ));
+
+        $this->assertSame($imagick->getImageWidth(), 50);
+        $this->assertSame($imagick->getImageHeight(), 74);
+        $imagick->clear();
+
+
+        // width less, reduceOnly off
+        $imagick->readImage($file); //101x149
+        $sampler->convertImagick($imagick, array(
+            'width'      => 150,
+            'reduceOnly' => false
+        ));
+
+        $this->assertSame($imagick->getImageWidth(), 150);
+        $this->assertSame($imagick->getImageHeight(), 221);
+        $imagick->clear();
+
+        // not less, reduceOnly off
+        $imagick->readImage($file); //101x149
+        $sampler->convertImagick($imagick, array(
+            'width'      => 50,
+            'reduceOnly' => false
+        ));
+
+        $this->assertSame($imagick->getImageWidth(), 50);
+        $this->assertSame($imagick->getImageHeight(), 74);
+        $imagick->clear();
+    }
+
+    public function testReduceOnlyByHeightWorks()
+    {
+        $sampler = new Autowp_Image_Sampler();
+
+        $file = dirname(__FILE__) . '/_files/Towers_Schiphol_small.jpg';
+
+        $imagick = new Imagick();
+
+        // height less
+        $imagick->readImage($file); //101x149
+        $sampler->convertImagick($imagick, array(
+            'height'     => 200,
+            'reduceOnly' => true
+        ));
+
+        $this->assertSame($imagick->getImageWidth(), 101);
+        $this->assertSame($imagick->getImageHeight(), 149);
+        $imagick->clear();
+
+        // not less
+        $imagick->readImage($file); //101x149
+        $sampler->convertImagick($imagick, array(
+            'height'     => 100,
+            'reduceOnly' => true
+        ));
+
+        $this->assertSame($imagick->getImageWidth(), 68);
+        $this->assertSame($imagick->getImageHeight(), 100);
+        $imagick->clear();
+
+        // height less, reduceOnly off
+        $imagick->readImage($file); //101x149
+        $sampler->convertImagick($imagick, array(
+            'height'     => 200,
+            'reduceOnly' => false
+        ));
+
+        $this->assertSame($imagick->getImageWidth(), 136);
+        $this->assertSame($imagick->getImageHeight(), 200);
+        $imagick->clear();
+
+        // not less, reduceOnly off
+        $imagick->readImage($file); //101x149
+        $sampler->convertImagick($imagick, array(
+            'height'     => 100,
+            'reduceOnly' => false
+        ));
+
+        $this->assertSame($imagick->getImageWidth(), 68);
+        $this->assertSame($imagick->getImageHeight(), 100);
+        $imagick->clear();
+    }
 }
 
 if (PHPUnit_MAIN_METHOD == 'Autowp_Image_SamplerTests::main') {
